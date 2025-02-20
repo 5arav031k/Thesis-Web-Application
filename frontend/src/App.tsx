c5bb48308be3f2938b3ec148bf2fd9e5e4b5b668
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-02-20 14:53:38 +0100

    add all together

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Sidebar} from "./components/sidebar/Sidebar.tsx";
+import {ActionButtons} from "./components/actionbuttons/ActionButtons.tsx";
+import {Separator} from "./components/separator/Separator.tsx";
+import {BranchesTable} from "./components/branches/BranchesTable.tsx";
+import {Header} from "./components/header/Header.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header/>
+      <div className="content-container">
+          <Sidebar/>
+          <div className="content">
+              <div className="content-header">Branches</div>
+              <ActionButtons/>
+              <Separator/>
+              <BranchesTable/>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
