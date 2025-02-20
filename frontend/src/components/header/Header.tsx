555ec49d5fec1203ff671fd9e4cf573657562628
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-02-20 14:50:35 +0100

    add header

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import WaffleIcon from '../../assets/waffle-icon.svg';
+
+export const Header = () => (
+    <>
+        <div className="header-container">
+            <div className="aris-container">
+                <div className="waffle-container">
+                    <img src={WaffleIcon} className="waffle-icon" alt="waffle-icon"/>
+                </div>
+                <div className="aris-name">
+                    <span className="aris">ARIS</span>
+                    Ta Service
+                </div>
+            </div>
+        </div>
+    </>
+)
