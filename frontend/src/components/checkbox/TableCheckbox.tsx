977600e2126d21cd2643f79bf2bb03ddc1b23a55
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-01 14:18:12 +0100

    add checkboxes for the table

---
+++ frontend/src/components/checkbox/TableCheckbox.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+interface TableCheckboxProps {
+  launchId: number;
+  selectedLaunches: number[];
+  setSelectedLaunches: React.Dispatch<React.SetStateAction<number[]>>;
+  hoveredLaunchRow: number | null;
+  setHoveredLaunchRow: (id: number | null) => void;
+}
+
+export const TableCheckbox: React.FC<TableCheckboxProps> = ({
+  launchId,
+  selectedLaunches,
+  setSelectedLaunches,
+  hoveredLaunchRow,
+  setHoveredLaunchRow,
+}) => {
+  const [hoveredSvgId, setHoveredSvgId] = useState<number | React.SetStateAction<null>>(null);
+
+  const toggleCheckbox = (id: number) => {
+    setSelectedLaunches((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id],
+    );
+  };
+
+  return (
+    <div
+      className="checkbox branches-table-cell"
+      onMouseEnter={() => setHoveredLaunchRow(launchId)}
+      onMouseLeave={() => setHoveredLaunchRow(null)}
+    >
+      <svg
+        width="21"
+        height="21"
+        onClick={() => toggleCheckbox(launchId)}
+        onMouseEnter={() => setHoveredSvgId(launchId)}
+        onMouseLeave={() => setHoveredSvgId(null)}
+        className={
+          hoveredLaunchRow === launchId || selectedLaunches.includes(launchId) ? 'visible' : ''
+        }
+        style={{ cursor: 'pointer' }}
+      >
+        <use
+          href={`#${
+            selectedLaunches.includes(launchId)
+              ? 'checkbox-checked'
+              : hoveredSvgId === launchId
+                ? 'checkbox-hovered'
+                : 'checkbox-default'
+          }`}
+        />
+      </svg>
+    </div>
+  );
+};
