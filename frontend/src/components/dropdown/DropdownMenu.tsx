04e34660fe3b96e248850faef547dae09be3a1bc
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-04 15:32:54 +0100

    small refactor

--- frontend/src/components/dropdown/DropdownMenu.tsx
+++ frontend/src/components/dropdown/DropdownMenu.tsx
@@ -7,18 +7,31 @@ import './dropdownElements.css';
 interface DropdownMenuProps {
   labelName: string;
   items: { value: number; label: string }[];
+  setSelectedItems: React.Dispatch<React.SetStateAction<any[]>>;
+  ref: any;
 }
 
-export const DropdownMenu: React.FC<DropdownMenuProps> = ({ labelName, items }) => {
+export const DropdownMenu: React.FC<DropdownMenuProps> = ({
+  labelName,
+  items,
+  setSelectedItems,
+  ref,
+}) => {
+  const handleChange = (selectedOptions: any) => {
+    setSelectedItems(selectedOptions);
+  };
+
   return (
     <div className={styles.dropdownContainer}>
       <Label className={styles.dropdownLabel}>{labelName}</Label>
       <Select
+        ref={ref}
         classNamePrefix="custom-select"
         closeMenuOnSelect={false}
         options={items}
         menuPlacement="bottom"
         isMulti
+        onChange={handleChange}
         theme={(theme) => ({
           ...theme,
           borderRadius: 2,
