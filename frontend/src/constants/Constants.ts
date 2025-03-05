1fcc9fd90f921d915b3af3efeb65e817b28d823e
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-05 14:52:14 +0100

    add constants

---
+++ frontend/src/constants/Constants.ts
@@ -0,0 +1,8 @@
+export const BRANCH_DEFAULTS = {
+  name: '//aris-all/',
+  testedVersion: '99.0.0.0-SNAPSHOT',
+  runtimeVersion: '2025.2.3-stable',
+  devMode: false,
+  fromJenkins: true,
+  templateSelector: 'NOSYNC',
+} as const;
