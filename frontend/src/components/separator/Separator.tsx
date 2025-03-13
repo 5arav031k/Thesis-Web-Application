9e493acd1a5b41ab43dfdf0e976aa350276631a1
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-13 20:50:37 +0100

    modify separator component using fluent-ui library

--- frontend/src/components/separator/Separator.tsx
+++ frontend/src/components/separator/Separator.tsx
@@ -1,5 +1,3 @@
-export const Separator = () => (
-  <>
-    <div className="separator"></div>
-  </>
-);
+import styles from './separator.module.css';
+
+export const Separator = () => <div className={styles.separator}></div>;
