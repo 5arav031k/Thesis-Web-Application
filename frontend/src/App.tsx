8d59c49487d08b6261d8c34d344ca6bc48c7603c
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-13 20:51:30 +0100

    modify main file with new components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { Sidebar } from './components/sidebar/Sidebar.tsx';
-import { ActionButtons } from './components/actionbuttons/ActionButtons.tsx';
 import { Separator } from './components/separator/Separator.tsx';
 import { BranchesTable } from './components/branches/BranchesTable.tsx';
 import { Header } from './components/header/Header.tsx';
+import { ActionButtons } from './components/actionbuttons/ActionButtons.tsx';
 import { useState } from 'react';
+import { SelectableItem } from './model/SelectableItem.ts';
 
 const App = () => {
-  const [selectedLaunches, setSelectedLaunches] = useState<number[]>([]);
+  const [selectedItems, setSelectedItems] = useState<SelectableItem[]>([]);
 
   return (
     <>
@@ -15,13 +16,10 @@ const App = () => {
       <div className="content-container">
         <Sidebar />
         <div className="content">
-          <div className="content-header">Branches</div>
-          <ActionButtons selectedLaunches={selectedLaunches} />
+          <div className="content-header">Launches</div>
+          <ActionButtons selectedItems={selectedItems} />
           <Separator />
-          <BranchesTable
-            selectedLaunches={selectedLaunches}
-            setSelectedLaunches={setSelectedLaunches}
-          />
+          <BranchesTable setSelectedItems={setSelectedItems} />
         </div>
       </div>
     </>
