07f259930b2f11b8db8588cb0f17475ba7bb2635
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-13 20:49:52 +0100

    modify header component using fluent-ui library

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,14 +1,15 @@
 import WaffleIcon from '../../assets/waffle-icon.svg';
+import styles from './header.module.css';
 
 export const Header = () => (
   <>
-    <div className="header-container">
-      <div className="aris-container">
-        <div className="waffle-container">
-          <img src={WaffleIcon} className="waffle-icon" alt="waffle-icon" />
+    <div className={styles.headerContainer}>
+      <div className={styles.arisContainer}>
+        <div className={styles.waffleContainer}>
+          <img src={WaffleIcon} className={styles.waffleIcon} alt="waffle-icon" />
         </div>
-        <div className="aris-name">
-          <span className="aris">ARIS</span>
+        <div className={styles.arisName}>
+          <span className={styles.aris}>ARIS</span>
           Ta Service
         </div>
       </div>
