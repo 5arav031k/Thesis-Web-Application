31f1b68be1b00e7ef2b4309e91a6ee636a6d90b2
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-13 20:50:50 +0100

    modify sidebar component using fluent-ui library

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,29 @@
 import BranchesIcon from '../../assets/branches-icon.svg';
+import LaunchesIcon from '../../assets/launches-icon.svg';
+import styles from './sidebar.module.css';
+import { useState } from 'react';
 
-export const Sidebar = () => (
-  <>
-    <div className="sidebar-container">
-      <div className="sidebar-button-container">
-        <div className="sidebar-button">
-          <img src={BranchesIcon} className="branches-icon" alt="Branches logo" />
+export const Sidebar = () => {
+  const [selected, setSelected] = useState<string>('launches');
+
+  return (
+    <div className={styles.sidebarContainer}>
+      <div className={styles.sidebarButtonContainer}>
+        <div
+          className={`${styles.sidebarButton} ${selected === 'launches' ? styles.selected : ''}`}
+          onClick={() => setSelected('launches')}
+        >
+          <img src={LaunchesIcon} className="icon" alt="Launches logo" />
+          Launches
+        </div>
+        <div
+          className={`${styles.sidebarButton} ${selected === 'branches' ? styles.selected : ''}`}
+          onClick={() => setSelected('branches')}
+        >
+          <img src={BranchesIcon} className="icon" alt="Branches logo" />
           Branches
         </div>
       </div>
     </div>
-  </>
-);
+  );
+};
