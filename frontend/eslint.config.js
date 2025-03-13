fb02d0e7b6cdb3434fc37741ea85bea0995654a7
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-03-13 20:53:13 +0100

    add new library

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,45 +5,49 @@ import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+import globals from "globals";
 
 export default [
-  {
-    ignores: ['node_modules/', 'dist/'],
-    files: ['src/**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+    {
+        ignores: ['node_modules/', 'dist/'],
+        files: ['src/**/*.{ts,tsx,js,jsx}'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+            globals: {
+                ...globals.node,
+            }
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            react: eslintPluginReact,
+            'react-hooks': eslintPluginReactHooks,
+            import: eslintPluginImport,
+            'jsx-a11y': eslintPluginJsxA11y,
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            'no-undef': 'warn',
+            'import/prefer-default-export': 'off',
+            'import/no-named-as-default': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.jsx'] }],
+            'prettier/prettier': 'warn',
+            'react/function-component-definition': [
+                2,
+                { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+            ],
+            'react/prop-types': 'off',
+            'no-else-return': 'off',
+            'no-nested-ternary': 'off',
+        },
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      react: eslintPluginReact,
-      'react-hooks': eslintPluginReactHooks,
-      import: eslintPluginImport,
-      'jsx-a11y': eslintPluginJsxA11y,
-      prettier: eslintPluginPrettier,
-    },
-    rules: {
-      'no-undef': 'warn',
-      'import/prefer-default-export': 'off',
-      'import/no-named-as-default': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.jsx'] }],
-      'prettier/prettier': 'warn',
-      'react/function-component-definition': [
-        2,
-        { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
-      ],
-      'react/prop-types': 'off',
-      'no-else-return': 'off',
-      'no-nested-ternary': 'off',
-    },
-  },
 ];
