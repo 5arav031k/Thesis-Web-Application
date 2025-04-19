da67b1727f8c32e31180c8c805d51e4e110246a6
Roman Yehorov <roman.yehorov@student.tuke.sk>
2025-04-19 15:08:50 +0200

    feat: add app config

---
+++ backend/src/main/java/com/thesis/yr/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.thesis.yr.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
